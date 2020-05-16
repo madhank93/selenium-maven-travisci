e28506f6a4f90b28db31297b7e036402f345fee6
madhank93 <maddymadhan946@gmail.com>
2020-05-16 18:25:49 +0530

    adding a method to capture screenshot and removing unwanted tag from travis.yml

--- src/test/java/io/github/madhank93/selenium_maven_travisci/SimpleTest.java
+++ src/test/java/io/github/madhank93/selenium_maven_travisci/SimpleTest.java
@@ -1,9 +1,14 @@
 package io.github.madhank93.selenium_maven_travisci;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -34,11 +39,22 @@ public class SimpleTest {
         }
     }
 
-    @Test
+    @Test (priority = 1)
     public void test() {
     	driver.get("https://time.is/");
     	String currentDate = driver.findElement(By.xpath("//div[@id='dd']")).getText();
     	System.out.println("Date: " + currentDate);
+    }
+    
+    @Test (priority = 2)
+    public void screenshot() {
+    	try {
+    		TakesScreenshot screenShot = (TakesScreenshot) driver;
+    		FileHandler.copy(screenShot.getScreenshotAs(OutputType.FILE),
+    				new File(System.getProperty("user.dir") + "/screenshot" + ".png"));
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
     }
 
 }
