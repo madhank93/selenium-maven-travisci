98aae59c2acb45b76cf29df88c4a056ef5e19fb3
madhank93 <maddymadhan946@gmail.com>
2020-05-16 16:46:52 +0530

    removing 'disbale gpu' from chrome options

--- src/test/java/io/github/madhank93/selenium_maven_travisci/GoogleTest.java
+++ src/test/java/io/github/madhank93/selenium_maven_travisci/GoogleTest.java
@@ -24,7 +24,6 @@ public class GoogleTest {
     public void setupTest() {
     	ChromeOptions options = new ChromeOptions();
     	options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
         driver = new ChromeDriver(options);
     }
 
